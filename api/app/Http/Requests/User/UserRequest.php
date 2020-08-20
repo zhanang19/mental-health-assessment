ec5c89bb7e1ec3d14460d71dc6ec1eede6fe1ba8
Carlo Miguel Dy <carlomigueldy@gmail.com>
2020-08-20 11:38:51 +0800

    feat(persist): UserRequest persist() method added to handle POST and PUT requests inside the class itself

---
+++ api/app/Http/Requests/User/UserRequest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use App\Enums\UserRoles;
+use App\Repository\UserRepositoryInterface;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Arr;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * @var UserRepositoryInterface
+     */
+    protected $userRepository;
+
+    /**
+     * @param UserRepositoryInterface $userRepository
+     */
+    public function __construct(UserRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->check() && auth()->user()->role != UserRoles::Student;
+    }
+
+    /**
+     * @param int $userId
+     * @return User
+     */
+    public function persist($userId = null)
+    {
+        $user = null;
+
+        if ($this->method() == "POST") {
+            $user = $this->userRepository->create($this->all());
+        } else if ($this->method() == "PUT") {
+            $user = $this->userRepository->update($userId, $this->all());
+        }
+
+        return $user;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $rules = [
+            'username' => 'required',
+            'first_name' => 'required',
+            'last_name' => 'required',
+            // 'middle_name' => 'required',
+            'email' => 'required|email',
+            'contact_number' => 'required',
+            'role' => 'required',
+            'type' => 'required',
+            'date_joined' => 'required',
+            'password' => 'required|confirmed',
+        ];
+
+        if ($this->method() == "PUT") {
+            $rules = Arr::except($rules, [
+                'password'
+            ]);
+        }
+
+        return $rules;
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'username' => 'required',
+            'first_name' => 'first name',
+            'last_name' => 'last name',
+            'middle_name' => 'middle name',
+            'contact_number' => 'contact number',
+            'role' => 'role',
+            'type' => 'user type',
+            'date_joined' => 'date joined',
+        ];
+    }
+}
