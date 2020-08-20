73e25479a6388242769785295bdfed2bdae9e57b
Carlo Miguel Dy <carlomigueldy@gmail.com>
2020-08-20 11:40:08 +0800

    feat(StoreTypes): added store types to prevent hardcoding store action types

---
+++ web/utils/StoreTypes.js
@@ -0,0 +1,22 @@
+export const UserActions = Object.freeze({
+  FETCH_ALL: "users/FETCH_ALL",
+  FETCH: "users/FETCH",
+  CREATE: "users/CREATE",
+  UPDATE: "users/UPDATE",
+  UPDATE_AVATAR: "users/UPDATE_AVATAR",
+  DELETE: "users/DELETE",
+  RESTORE: "users/RESTORE",
+  PERMANENTLY_DELETE: "users/PERMANENTLY_DELETE",
+})
+
+export const SurveyActions = Object.freeze({
+  FETCH_ALL: "surveys/FETCH_ALL",
+  FETCH_BY_SLUG: "surveys/FETCH_BY_SLUG",
+  FETCH: "surveys/FETCH",
+  CREATE: "surveys/CREATE",
+  UPDATE: "surveys/UPDATE",
+  UPDATE_AVATAR: "surveys/UPDATE_AVATAR",
+  DELETE: "surveys/DELETE",
+  RESTORE: "surveys/RESTORE",
+  PERMANENTLY_DELETE: "surveys/PERMANENTLY_DELETE",
+})
