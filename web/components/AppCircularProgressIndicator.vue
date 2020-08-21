207a69a8372b854dbbc56777dc9b8d88ec3eb6eb
Carlo Miguel Dy <carlomigueldy@gmail.com>
2020-08-21 12:56:15 +0800

    feat: created a progress indicator component

---
+++ web/components/AppCircularProgressIndicator.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-card-text class="d-flex justify-center align-center fill-height">
+      <div class="text-center pa-4 mt-1">
+        <v-progress-circular indeterminate :color="colorTheme === 'white' ? 'primary' : 'white'"></v-progress-circular>
+      </div>
+    </v-card-text>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    colorTheme: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
