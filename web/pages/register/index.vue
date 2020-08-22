73e09b3a8598bcb794e4a66b478870d0117c6e1c
Carlo Miguel Dy <carlomigueldy@gmail.com>
2020-08-22 17:28:52 +0800

    ...

--- web/pages/register/index.vue
+++ web/pages/register/index.vue
@@ -23,6 +23,28 @@
         </v-col>
       </v-row>
     </v-container>
+
+    <v-tooltip top>
+      <template #activator="{ on, attrs }">
+        <v-btn
+          style="z-index: 999"
+          v-on="on"
+          v-bind="attrs"
+          @click="$router.back()"
+          class="primary--text"
+          elevation="10"
+          bottom
+          left
+          fixed
+          rounded
+          x-large
+        >
+          <v-icon left>mdi-arrow-left</v-icon>
+          <span>Back</span>
+        </v-btn>
+      </template>
+      <span>Go back to Login screen</span>
+    </v-tooltip>
   </v-main>
 </template>
 
