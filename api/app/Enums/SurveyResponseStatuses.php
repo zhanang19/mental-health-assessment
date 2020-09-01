7508b28ff2e08563c8303a442be5fde08b939fa8
Carlo Miguel Dy <carlomigueldy@gmail.com>
2020-09-01 09:12:35 +0800

    feat: created Enums for survey respones statuses

---
+++ api/app/Enums/SurveyResponseStatuses.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+final class SurveyResponseStatuses extends Enum
+{
+    const InProgress = "In Progress";
+    const Completed = "Completed";
+}
